cab7f9c75e594f306d64854880387af12bae158e
Goes <goesmodul@gmail.com>
2023-07-19 09:18:26 +0700

    JPA example

---
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernete/LearnJpaAndHiberneteApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnjpaandhibernete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHiberneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHiberneteApplication.class, args);
+	}
+
+}
